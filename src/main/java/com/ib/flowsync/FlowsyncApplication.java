c218cbcc1c344d1c85f7dab834fa319ce345e2c5
Jeremy Roussy <je.roussy@gmail.com>
2024-11-18 10:15:42 +0100

    project setup

---
+++ src/main/java/com/ib/flowsync/FlowsyncApplication.java
@@ -0,0 +1,11 @@
+package com.ib.flowsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowsyncApplication {
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(FlowsyncApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ib/flowsync/FlowsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib.flowsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
