05b89a5a89540230a4d38cf1bc9ef223f0d0bf7d
Kbenard <kevin.benard00@gmail.com>
2024-12-03 08:55:05 +0100

    Add ProductDTO

--- src/main/java/com/ib/flowsync/service/ProductService.java
+++ src/main/java/com/ib/flowsync/service/ProductService.java
@@ -1,12 +1,17 @@
 package com.ib.flowsync.service;
 
+import com.ib.flowsync.dto.ProductDTO;
 import com.ib.flowsync.entity.Category;
 import com.ib.flowsync.entity.Product;
+import com.ib.flowsync.entity.Supplier;
+import com.ib.flowsync.repository.CategoryRepository;
 import com.ib.flowsync.repository.ProductRepository;
+import com.ib.flowsync.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -14,8 +19,25 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public void createProduct(Product product) {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public void createProduct(ProductDTO productDTO) {
+        Product product = new Product();
         product.setId(null);
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setVat(productDTO.getVat());
+
+        Optional<Category> category = categoryRepository.findById(productDTO.getCategoryId());
+        category.ifPresent(product::setCategory);
+
+        Optional<Supplier> supplier = supplierRepository.findById(productDTO.getSupplierId());
+        supplier.ifPresent(product::setSupplier);
+
         productRepository.save(product);
     }
 
@@ -35,9 +57,23 @@ public class ProductService {
         return (List<Product>) productRepository.findByCategory(category);
     }
 
-    public void updateProduct(Product product, Integer productId) {
-        product.setId(productId);
-        productRepository.save(product);
+    public void updateProduct(ProductDTO productDTO, Integer productId) {
+        Optional<Product> existingProductOpt = productRepository.findById(productId);
+
+        if (existingProductOpt.isPresent()) {
+            Product product = existingProductOpt.get();
+            product.setName(productDTO.getName());
+            product.setPrice(productDTO.getPrice());
+            product.setVat(productDTO.getVat());
+
+            Optional<Category> category = categoryRepository.findById(productDTO.getCategoryId());
+            category.ifPresent(product::setCategory);
+
+            Optional<Supplier> supplier = supplierRepository.findById(productDTO.getSupplierId());
+            supplier.ifPresent(product::setSupplier);
+
+            productRepository.save(product);
+        }
     }
 
     public void deleteProduct(Integer productId) {
